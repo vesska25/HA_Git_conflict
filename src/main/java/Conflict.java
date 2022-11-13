f60277056d49e13c4649e9d18c6691af5d094131
Sergei Savich <sergesvc@gmail.com>
2022-11-13 17:40:13 +0300

    added new lines

--- src/main/java/Conflict.java
+++ src/main/java/Conflict.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Conflict {
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("Let's make a conflict");
+        System.out.println("I want a conflict");
+    }
 }
